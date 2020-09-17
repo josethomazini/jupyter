47c3c89978f010635db4cd9c1b3f4190ee81099b
Jose Francisco Brand Thomazini <jose.thomazini@gmail.com>
2020-09-16 22:45:53 -0300

    change the defaut location of the notebooks to the volume, and add license to the docker ignored

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import configparser
 
 
-# Read secret hash from volume on /home/pyuser/secret/
+# Read secret hash from the volume on /home/pyuser/secret/
 config = configparser.ConfigParser()
 config.read('/home/pyuser/secret/secret.ini')
 jupyter_hash = config['DEFAULT']['JUPYTER_HASH']
@@ -11,3 +11,6 @@ config = get_config()
 
 # Define the hashed password on Jupyter
 config.NotebookApp.password = str(jupyter_hash)
+
+# Define the location of the notebooks to be saved in the data volume
+config.NotebookApp.notebook_dir = '/home/pyuser/data'
